f9caa866ae6d5b97b76463a73ff25b05f4e3a554
natthaphon.a <natthaphon.a@takeitsolution.com>
2024-08-27 07:41:22 +0000

    testt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,14 @@ export class AppComponent {
   title = 'angular';
 }
 
-function foo(x: number, y: string = "default", z?: number) {
-  // ...
-}
+var { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 1024,  // Noncompliant
+  publicKeyEncoding: { type: 'spki', format: 'pem' },
+  privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+}); // Noncompliant: 1024 bits is too short for a RSA key pair
 
-foo(42, undefined);
+crypto.generateKeyPair('ec', {
+  namedCurve: 'secp112r2',
+  publicKeyEncoding: { type: 'spki', format: 'pem' },
+  privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+}, callback);
