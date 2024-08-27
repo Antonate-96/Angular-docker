7a263ec7a53674830625b1437e7b4dea097a5f0f
natthaphon.a <natthaphon.a@takeitsolution.com>
2024-08-27 07:59:51 +0000

    addd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,3 @@ export class AppComponent {
   title = 'angular';
 }
 
-var { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-  modulusLength: 1024,  // Noncompliant
-  publicKeyEncoding: { type: 'spki', format: 'pem' },
-  privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
-}); // Noncompliant: 1024 bits is too short for a RSA key pair
-
-crypto.generateKeyPair('ec', {
-  namedCurve: 'secp112r2',
-  publicKeyEncoding: { type: 'spki', format: 'pem' },
-  privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
-}, callback);
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,3 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
 
-function whoCalled() {
-  if (arguments.caller == null)   //Noncompliant
-    console.log('I was called from the global scope.');
-  else
-    console.log(arguments.caller + ' called me!');  // Noncompliant
-
-  console.log(whoCalled.caller);  // Noncompliant
-  console.log(whoCalled.arguments);  // Noncompliant
-}
