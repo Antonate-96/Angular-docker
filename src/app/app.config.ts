b234898307d00260a09d86b28ccbd749e8bf367e
natthaphon.a <natthaphon.a@takeitsolution.com>
2024-08-27 07:50:52 +0000

    testt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,3 +6,13 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
+
+function whoCalled() {
+  if (arguments.caller == null)   //Noncompliant
+    console.log('I was called from the global scope.');
+  else
+    console.log(arguments.caller + ' called me!');  // Noncompliant
+
+  console.log(whoCalled.caller);  // Noncompliant
+  console.log(whoCalled.arguments);  // Noncompliant
+}
