72795af0c7e3fc114b5726adb71b0ded4b4aff4e
natthaphon.a <natthaphon.a@takeitsolution.com>
2024-08-29 11:40:50 +0700

    test --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular';
+  example = {
+    "systemId": "test",
+    "userName": "test001",
+    "password": "12345678",
+    "ipAddress": "10.1.1.1",
+    "requestData": {
+      "OFFCODE": "100400",
+      "INC_CODE": "410070",
+      "BEGIN_DATE": ""
+    }
+  }
 }
 
